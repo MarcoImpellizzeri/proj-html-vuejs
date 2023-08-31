5dc6035e7db08c17b3898aee46b783282b38aa5f
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-08-31 16:57:52 +0200

    TheHeader 1.1

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,20 @@
 <script >
 export default {
+    data() {
+        return {
+            navItem: [
+                { label: 'Home' },
+                { label: 'Service', children: true, drop: 'dropdown' },
+                { label: 'About', children: true, drop: 'dropdown' },
+                { label: 'Videos', children: true, drop: 'dropdown' },
+                { label: 'Blog' },
+                { label: 'Store', children: true, drop: 'dropdown' }
+            ],
+        }
+    },
+    methods: {
 
+    }
 }
 </script>
 
@@ -17,26 +31,41 @@ export default {
 
 
                 <!-- navbar -->
-
                 <div class="navbar-container d-flex justify-content-between align-items-center">
                     <ul class="list-unstyled d-flex">
-                        <li><a href="#0">Home</a></li>
-                        <li>Service <span><i class="fa-solid fa-chevron-down"></i></span></li>
-                        <li>About <span><i class="fa-solid fa-chevron-down"></i></span></li>
-                        <li>Videos <span><i class="fa-solid fa-chevron-down"></i></span></li>
-                        <li>Blog</li>
-                        <li>
-                            Store <span class="badge text-bg-warning">New</span>
-                            <span><i class="fa-solid fa-chevron-down"></i></span>
+                        <li v-for="(item, i) in navItem" :key="`item${i}`">
+                            <div :class="item.drop">
+                                <a href="#0" class="dropa">{{ item.label }}
+                                    <span v-if="item.label === 'Store'" class="badge text-bg-warning">New</span>
+                                    <span v-if="item.children"><i class="fa-solid fa-chevron-down ms-2"></i></span>
+                                </a>
+                                <div v-if="item.label === 'Service'" class="dropdown-content">
+                                    <a href="#0">All Service</a>
+                                    <a href="#0">Service Single Page</a>
+                                </div>
+                                <div v-if="item.label === 'About'" class="dropdown-content">
+                                    <a href="#0">About me</a>
+                                    <a href="#0">Our Sponsor</a>
+                                    <a href="#0">Contact</a>
+                                </div>
+                                <div v-if="item.label === 'Videos'" class="dropdown-content">
+                                    <a href="#0">All Playlist</a>
+                                    <a href="#0">Playlist Page</a>
+                                    <a href="#0">Video Single Page</a>
+                                </div>
+                                <div v-if="item.label === 'Store'" class="dropdown-content">
+                                    <a href="#0">Simple Product</a>
+                                    <a href="#0">Variable Product</a>
+                                </div>
+                            </div>
                         </li>
                     </ul>
 
-                    <button class="btn btn-primary">Schedule a workout</button>
+                    <button class="my-btn">Schedule a workout</button>
 
-                    <div><i class="fa-solid fa-magnifying-glass"></i></div>
+                    <div><a href="#0"><i class="fa-solid fa-magnifying-glass mx-3"></i></a></div>
 
-                    <div><i class="fa-solid fa-cart-shopping"></i></div>
-
+                    <div><a href="#0"><i class="fa-solid fa-cart-shopping mx-3"></i></a></div>
                 </div>
             </div>
         </div>
@@ -44,9 +73,11 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use "../style/partials/variables" as *;
+
 .container-fluid {
     color: white;
-    background-color: black;
+    background-color: $prymary-color;
 
     .container {
         height: 100px;
@@ -54,18 +85,76 @@ export default {
         .navbar-container {
             height: 100%;
 
+            a {
+                color: white;
+                text-decoration: none;
+                padding: 40px 5px;
+            }
+
             ul {
                 margin: 0;
 
                 li {
-                    margin-left: 15px;
-                    margin-right: 15px;
+                    margin: 0 5px;
 
-                    a {
-                        color: white;
-                        text-decoration: none;
-                        padding: 20px 10px 20px 10px;
+                    .dropdown {
+                        position: relative;
+                        display: inline-block;
+
+                        &:hover .dropdown-content {
+                            opacity: 1;
+                            transition: opacity 0.6s;
+
+                        }
+
+                        .dropa {
+                            height: 100px;
+                            color: white;
+                            padding: 40px 16px;
+                            font-size: 16px;
+                            border: none;
+                        }
+
+                        .dropdown-content {
+                            opacity: 0;
+                            transition: opacity 0.3s ease-in-out;
+                            position: absolute;
+                            top: 62px;
+                            background-color: $prymary-color;
+                            min-width: 150px;
+                            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+                            z-index: 1;
+                            border-radius: 0 0 5px 5px;
+
+                            a {
+                                color: white;
+                                padding: 12px 16px;
+                                text-decoration: none;
+                                display: block;
+                                font-size: .8rem;
+
+                                &:last-child {
+                                    border-radius: 0 0 5px 5px;
+                                }
+                                
+
+                                &:hover {
+                                    transition: 0.7s;
+                                    background-color: $my-blu-color;
+                                }
+                            }
+                        }
                     }
+                }
+            }
+
+            .my-btn {
+                background-color: $my-blu-color;
+                font-size: .9rem;
+
+                &:hover {
+                background-color: $my-red-color;
+
                 }
             }
         }
